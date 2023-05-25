59a7bce0ccee37d9516db22ce32707fc1c6a0e9d
yuseung0429 <yuseung0429@naver.com>
2023-05-25 19:08:29 +0900

    Init: SaveThePets Backend Project 생성 Spring Boot Project 생성, 패키지 이름 설정

---
+++ src/main/java/com/savethepets/SaveThePetsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.savethepets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveThePetsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveThePetsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savethepets/SaveThePetsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savethepets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveThePetsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
