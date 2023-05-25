27f77f530cf5d40597310c7f5b38b12d6c8e820a
yuseung0429 <yuseung0429@naver.com>
2023-05-26 00:05:46 +0900

    Chore: Controller Endpoint 설정 Controller 9개 Endpoint 설정

--- src/main/java/com/savethepets/controller/CommentController.java
+++ src/main/java/com/savethepets/controller/CommentController.java
@@ -2,6 +2,32 @@ package com.savethepets.controller;
 
 import com.savethepets.service.CommentServiceImpl;
 
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.savethepets.dto;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/comment")
+
 public class CommentController {
 	private final CommentServiceImpl commentService;
+	
+	@PostMapping("/create")
+	ResponseEntity<Boolean> createComment(@RequestBody CommentInfoDTO commentInfoDTO) {return new ResponseEntity<>(null, HttpStatus.OK);};
+	
+	@PutMapping("/update")
+	ResponseEntity<Boolean> updateComment(@RequestBody CommentInfoDTO commentInfoDTO) {return new ResponseEntity<>(null, HttpStatus.OK);};
+	
+	@DeleteMapping("/remove")
+	ResponseEntity<Boolean> removeComment(@RequestBody Long commentId) {return new ResponseEntity<>(null, HttpStatus.OK);};
 }
--- src/main/java/com/savethepets/controller/CommentReportController.java
+++ src/main/java/com/savethepets/controller/CommentReportController.java
@@ -1,7 +1,28 @@
-package com.savethepets.controller;
 
 import com.savethepets.service.CommentReportServiceImpl;
 
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.savethepets.dto;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/commentreport")
+
 public class CommentReportController {
 	private final CommentReportServiceImpl commentReportService;
+	
+	@PostMapping("/create")
+	ResponseEntity<Boolean> createPostReport(@RequestBody CommentReportDTO commentReportDTO) {return new ResponseEntity<>(null, HttpStatus.OK);};
+	
+	@PutMapping("/update")
+	ResponseEntity<Boolean> updatePostReport(@RequestBody CommentReportDTO commentReportDTO) {return new ResponseEntity<>(null, HttpStatus.OK);};
 }
