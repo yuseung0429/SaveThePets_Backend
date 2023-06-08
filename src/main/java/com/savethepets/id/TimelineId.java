6f40b27a4c17440742b1f9e4f39450ae1df7e3a6
kj0513 <kwon980513@gmail.com>
2023-06-08 20:39:38 +0900

    Feat: Timeline Controller,Service,Repository 구현  - 구현 : createTimeline, removeTimeline

--- src/main/java/com/savethepets/id/TimelineId.java
+++ src/main/java/com/savethepets/id/TimelineId.java
@@ -5,12 +5,14 @@ import java.io.Serializable;
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Embeddable
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class TimelineId implements Serializable {
 	Long missingPostId;
 	Long sightingPostId;
